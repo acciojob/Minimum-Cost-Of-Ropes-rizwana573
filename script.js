bc4943119423fd0dc3de2f4bd9163e02ba940814
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-02 20:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ function calculateMinCost() {
 	let arr = document.getElementById("rope-lengths").value.split(",").map(Number);
     // console.log(nums, nums);
 	//const arr = arr.push();
-  console.log("arr11 ", arr);
+  //console.log("arr11 ", arr);
 	arr.sort(function(a,b){return a-b;});
+	let res=0;
   while(arr.length > 1){
 	  
-	  console.log("arr33 ", arr);
-	  let minCost = (arr[0])+(arr[1]);
-	  arr.shift();
-	  arr.shift(); 
-	  arr.push(minCost);
+	 // console.log("arr33 ", arr);
+	 let first = arr.shift();
+	  let second = arr.shift(); 
+	 res+=first+second;
+	  arr.push(first+second);
 	  arr.sort(function(a,b){return a-b;});
-	   console.log("arr22 ", arr); 
+	  // console.log("arr22 ", arr); 
   }
-	document.getElementById("result").innerHTML=arr[0];
+	document.getElementById("result").innerHTML=res;
 	
 }  
